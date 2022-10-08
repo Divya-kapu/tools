4c4e9b7ecfc6d3674a96a2b06c4fc388ac98f3b7
Divya-kapu <98731187+Divya-kapu@users.noreply.github.com>
2022-10-08 16:43:30 -0700

    Update main.py

--- key_logger/main.py
+++ key_logger/main.py
@@ -2,12 +2,7 @@ from pynput import keyboard
 
 
 def keystroke(pressed_key):
-  try:
     print('key pressed: {0} '.format(pressed_key))
-
-  except AttributeError:
-    print('invalid key pressed: {0}'.format(pressed_key))
-
 
 # Collect events
 with keyboard.Listener(on_press=keystroke) as listener:
